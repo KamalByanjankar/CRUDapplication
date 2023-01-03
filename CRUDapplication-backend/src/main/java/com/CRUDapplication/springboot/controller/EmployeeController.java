00db7e65b7d4a1a05ce80872891056ba855e6d65
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2023-01-03 16:09:23 +0100

    CORS origin added

--- CRUDapplication-backend/src/main/java/com/CRUDapplication/springboot/controller/EmployeeController.java
+++ CRUDapplication-backend/src/main/java/com/CRUDapplication/springboot/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.CRUDapplication.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.CRUDapplication.springboot.exception.ResourceNotFoundException;
 import com.CRUDapplication.springboot.model.Employee;
 import com.CRUDapplication.springboot.repository.EmployeeRepository;
 
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
