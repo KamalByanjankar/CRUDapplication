4494c89cc5eed437d657c4e6b64ebcd8135a061f
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2023-01-05 10:52:18 +0100

    delete method implemented

--- crudapplication-frontend/src/components/EmployeeList/EmployeeList.jsx
+++ crudapplication-frontend/src/components/EmployeeList/EmployeeList.jsx
@@ -6,16 +6,25 @@ import './EmployeeList.css'
 function EmployeeList() {
     const [employee, setEmployee] = useState([])
 
+    //fetch data from database
     const getDataFromServer = async () => {
         const response = await axios.get("http://localhost:8080/api/v1/employees")
         if(response && response.data){
             setEmployee(response.data)
         }
-    }
+    }    
     
     useEffect(() => {
         getDataFromServer();
     }, [])
+
+    //delete data from database
+    const deleteData = async (id) => {
+        const response = await axios.delete(`http://localhost:8080/api/v1/employees/${id}`)
+        if(response){
+            getDataFromServer();
+        }
+    }
 
   return (
     <div className="employeeList">
@@ -38,7 +47,7 @@ function EmployeeList() {
                                 <td>{data.emailId}</td>
                                 <td>
                                     <button>Edit</button>
-                                    <button>Delete</button>
+                                    <button onClick={() => deleteData(data.id)}>Delete</button>
                                 </td>
                             </tr>
                         </tbody>
